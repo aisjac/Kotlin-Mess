c1843243352a87da33b5a9e3a915dd3d9c008467
aisjac <[shaheenmirja143@gmail.com]>
2021-10-14 19:02:22 +0600

    slider design done

--- app/src/main/java/com/example/kotlinmess/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmess/MainActivity.kt
@@ -1,35 +1,83 @@
 package com.example.kotlinmess
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.ImageView
-import com.synnapps.carouselview.CarouselView
-import com.synnapps.carouselview.ImageListener
+import androidx.viewpager.widget.ViewPager
+import com.emon.customsliderwithindicator.ViewPagerAdapter.Pager_Adapter
+import com.emon.customsliderwithindicator.ViewPagerFragments.Fragment_One
+import com.emon.customsliderwithindicator.ViewPagerFragments.Fragment_Three
+import com.emon.customsliderwithindicator.ViewPagerFragments.Fragment_Two
+
 
 class MainActivity : AppCompatActivity() {
-    var carouselView: CarouselView? = null
 
-    var sampleImages = intArrayOf(
-        R.drawable.grp1,
-        R.drawable.grp2,
-        R.drawable.grp3
-    )
+    private lateinit var pagerview_adapter: Pager_Adapter
+    private lateinit var my_viewpaper: ViewPager
+    private lateinit var first_indicator: ImageView
+    private lateinit var second_indicator: ImageView
+    private lateinit var third_indicator: ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        carouselView = findViewById<View>(R.id.carouselView) as CarouselView
-        carouselView!!.setPageCount(sampleImages.size)
-        carouselView!!.setImageListener(imageListener)
+
+        my_viewpaper = findViewById(R.id.my_viewpaper)
+        first_indicator = findViewById(R.id.first_indicator)
+        second_indicator = findViewById(R.id.second_indicator)
+        third_indicator = findViewById(R.id.third_indicator)
+
+        pagerview_adapter = Pager_Adapter(supportFragmentManager)
+        add_MyFragment()
+        my_viewpaper.adapter = pagerview_adapter
+        my_viewpaper.addOnPageChangeListener(MyListener(this::onPageSelected))
+
+
     }
 
-    var imageListener: ImageListener = ImageListener { position, imageView ->
-        imageView.setImageResource(sampleImages[position])
-        imageView.scaleType = ImageView.ScaleType.FIT_CENTER
+    @SuppressLint("SetTextI18n")
+    private fun onPageSelected(position: Int) {
+        when (position) {
+            0 -> {
+                first_indicator.setImageResource(R.drawable.active_circle)
+                second_indicator.setImageResource(R.drawable.disable_circle)
+                third_indicator.setImageResource(R.drawable.disable_circle)
+
+            }
+            1 -> {
+                first_indicator.setImageResource(R.drawable.disable_circle)
+                second_indicator.setImageResource(R.drawable.active_circle)
+                third_indicator.setImageResource(R.drawable.disable_circle)
+
+            }
+            2 -> {
+                first_indicator.setImageResource(R.drawable.disable_circle)
+                second_indicator.setImageResource(R.drawable.disable_circle)
+                third_indicator.setImageResource(R.drawable.active_circle)
+            }
+        }
+    }
+
+
+    private fun add_MyFragment() {
+        pagerview_adapter.addFragment(Fragment_One())
+        pagerview_adapter.addFragment(Fragment_Two())
+        pagerview_adapter.addFragment(Fragment_Three())
+    }
+
+    class MyListener(private val closure: (Int) -> Unit) : ViewPager.OnPageChangeListener {
+        override fun onPageScrollStateChanged(p0: Int) {
+
+        }
+
+        override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
+
+        }
+
+        override fun onPageSelected(position: Int) = closure(position)
     }
 
 
